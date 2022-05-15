d1ffacb6a0015b977b583f79756cd259e1ef666e
Nata <volchekkk@yandex.ru>
2022-05-15 19:26:09 +0300

    D6

---
+++ NewsPaper/news/filters.py
@@ -0,0 +1,12 @@
+from django_filters import FilterSet
+from .models import New
+
+class NewFilter(FilterSet):
+    class Meta:
+        model = New
+        fields = {
+            'title': ['icontains'],
+            'author': ['gt'],
+            'newCategory__name': ['icontains'],
+            'dateCreation': ['lte'],
+        }
